5d247d2e5c0b9d661aceb9855a6dcb5b1dcc4ef4
seweed3 <matthewc1172@gmail.com>
2023-10-11 16:18:33 -0700

    Move ColorSensorTest to Testing Folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ColorSensorTest.java
@@ -1,11 +1,8 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp
 public class ColorSensorTest extends OpMode {
