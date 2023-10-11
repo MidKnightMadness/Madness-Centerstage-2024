abb9a86adf321b8bffc9442ab38d97e4dac0b459
seweed3 <matthewc1172@gmail.com>
2023-10-11 16:14:58 -0700

    Correct RGB Values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+@TeleOp
 public class ColorSensorTest extends OpMode {
 
     ColorSensor cs;
@@ -15,23 +17,29 @@ public class ColorSensorTest extends OpMode {
     public void init() {
         // Init
         cs = hardwareMap.get(ColorSensor.class, "test color sensor");
-        cs.resetDeviceConfigurationForOpMode();
+
 
     }
 
     @Override
     public void loop() {
         //color sensor is from 1 inch away
-        if (cs.red() >= 420 && cs.red() <= 440 && cs.green() > 800 && cs.green() < 825 && cs.blue() > 860 && cs.blue() < 890 && cs.alpha() > 690 && cs.alpha() < 720) {
+        if (cs.red() >= 430 && cs.red() <= 550 && cs.green() >= 860 && cs.green() <= 1000 && cs.blue() >= 920&& cs.blue() <= 1100) {
             //maximum and minimum for white color
             telemetry.addLine("Color: White");
         }
-        else if(cs.red()>=86&&cs.red()<=88&&cs.green()<=188&&cs.green()>=194&&cs.blue()>=390&&cs.blue()>=405&&cs.alpha()>=220&&cs.alpha()<=235){
+        else if(cs.red()>=60&&cs.red()<=120&&cs.green()>=150&&cs.green()<=240&&cs.blue()>=440&&cs.blue()<=550){
+            telemetry.addLine("Color: Blue");
+        }
+        else if(cs.red()>=150&&cs.red()<=260&&cs.green()>=120&&cs.green()<=220&&cs.blue()>=70&&cs.blue()<=170){
             telemetry.addLine("Color: Red");
         }
-        else if(cs.red()>=195&&cs.red()<=210&&cs.green()<155&&cs.green()>175&&cs.blue()<110&&cs.blue()>125&&cs.alpha()>155&&cs.alpha()>170){
-            telemetry.addLine("Color: Blue");
-
+        else{
+            telemetry.addLine("Color: Unidentified");
         }
+
+
+        telemetry.addLine("RGB Values/nRed: "+cs.red()+"/nGreen:" + cs.green()+"/nBlue:" + cs.blue()+ "/nAlpha:" + cs.alpha());
+        telemetry.update();
     }
 }
