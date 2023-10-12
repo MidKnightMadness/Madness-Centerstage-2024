2e6b457acea367581e3e0d6754945adaaafca684
seweed3 <matthewc1172@gmail.com>
2023-10-11 17:26:31 -0700

    Add arrays for easier acsess to RGB min and max values  for detecting each colorin ColorSensorTest.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorMRColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorMRColor.java
@@ -53,7 +53,7 @@ import java.util.ArrayList;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
-@TeleOp(name = "Sensor: Color", group = "Sensor")
+@TeleOp(name = "", group = "Sensor")
 public class SensorMRColor extends LinearOpMode {
 
   ColorSensor colorSensor;    // Hardware Device Object
