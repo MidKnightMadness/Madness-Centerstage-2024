56d38d59b369d1744ee41e71940f5beecd6d4cc4
shepbox <seojinroylee@gmail.com>
2023-11-08 17:28:25 -0800

    saving for now again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/AutonomousTestingPixelCycle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/AutonomousTestingPixelCycle.java
@@ -48,7 +48,7 @@ Opcode 3 and 4 are team color blue -> teamColor = 2
             {36, 12, 0},
             {84, 12, 0},
             //and repeat
-            {120, 36, 0},{84, 12, 0},{12, 12, 0}, {12, 36, 0}, {36, 12, 0}, {84, 12, 0},{120, 36, 0},{84, 12, 0},{12, 12, 0}, {12, 36, 0}, {36, 12, 0}, {84, 12, 0},
+            {120, 36, 0}, {84, 12, 0}, {12, 12, 0}, {12, 36, 0}, {36, 12, 0}, {84, 12, 0}, {120, 36, 0}, {84, 12, 0}, {12, 12, 0}, {12, 36, 0}, {36, 12, 0}, {84, 12, 0},
             //blue starting here
             //front blue also has {36, 132}
             {84, 132, 0},
@@ -59,7 +59,7 @@ Opcode 3 and 4 are team color blue -> teamColor = 2
             {36, 132, 0},
             {84, 132, 0},
             //and repeat
-            {84, 132, 0}, {120, 108, 0}, {84, 132, 0}, {12, 132, 0}, {12, 68, 0}, {36, 132, 0}, {84, 132, 0},{84, 132, 0}, {120, 108, 0}, {84, 132, 0}, {12, 132, 0}, {12, 68, 0}, {36, 132, 0}, {84, 132, 0}
+            {84, 132, 0}, {120, 108, 0}, {84, 132, 0}, {12, 132, 0}, {12, 68, 0}, {36, 132, 0}, {84, 132, 0}, {84, 132, 0}, {120, 108, 0}, {84, 132, 0}, {12, 132, 0}, {12, 68, 0}, {36, 132, 0}, {84, 132, 0}
 
 
     };
@@ -87,7 +87,7 @@ Opcode 3 and 4 are team color blue -> teamColor = 2
     boolean taskComplete = false; //just to make a few things run for now
     boolean overriding = false;
     boolean teamPropTask = false;
-    int nextTask =2; //1 is for intake, and 2 is for outtake
+    int nextTask = 2; //1 is for intake, and 2 is for outtake
 
     int stored = 1; //the starting pixel in robot
 
@@ -107,60 +107,84 @@ Opcode 3 and 4 are team color blue -> teamColor = 2
     public void loop() {
 
         //while the task at location is complete
-        if (teamPropTask == false) {
+        odometry.updatePosition();
+        PIDDrive.updatePID();
+        if (timer.time() < 23) {
+            if (teamPropTask == false) {
 
-        }
+                PIDDrive.setTargetState(teamProp.x, teamProp.y, teamColor * Math.PI / 2);
+                if (PIDDrive.distanceToTarget < 0.1) {
+                    teamPropTask = true;
+                    intake.setPower(-1);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    intake.setPower(0);
 
-        else{
-        if (targetStates[numberOfPointsReached][0] == 0 ||targetStates[numberOfPointsReached][0] == 1){ //arbitrary numbers check if it's one of the points where you run intake or outtake
-            taskComplete = false;
-        }
-
-        if (taskComplete == false) { //for both intake and outtake
-            if (nextTask == 1) {//if currently we're in staks
-                //intake
-                //stored++ for each intake
-
-                    taskComplete = true;
-                    nextTask = 2;
                 }
+
+            } else {
+                if (targetStates[numberOfPointsReached][0] == 0 || targetStates[numberOfPointsReached][0] == 1) { //arbitrary numbers check if it's one of the points where you run intake or outtake
+                    taskComplete = false;
+                }
+
+                if (taskComplete == false) { //for both intake and outtake
+                    if (nextTask == 1) {//if currently we're in staks
+                        //intake
+                        //stored++ for each intake
+
+                        taskComplete = true;
+                        nextTask = 2;
+                    }
+                } else {
+                    //outtake
+                    //stored-- each time
+                    if (stored == 0) {
+                        taskComplete = true;
+                        nextTask = 1;
+
+                    }
+                }
+                //help how do i
+
+
+                if (taskComplete == true) {
+
+                    if (PIDDrive.distanceToTarget < 0.1) { //when we reach the desired point, then our next point is queued
+                        numberOfPointsReached++;
+                    }
+                    //above is the exact same thing but when on backstage
+                    if (numberOfPointsReached < targetStates.length) { //movement
+
+                            PIDDrive.setTargetState(targetStates[numberOfPointsReached][0], targetStates[numberOfPointsReached][1], targetStates[numberOfPointsReached][2]);
+
+
+
+                    }
+
+                }
+            }
+
+        }else{
+            if (teamColor == 3){
+                PIDDrive.setTargetState(odometry.getXCoordinate(), 12, 0);
+                if (PIDDrive.distanceToTarget() < 0.1){
+                    PIDDrive.setTargetState(130, 12, 0);
+
+                }
+
             }
             else{
-                //outtake
-                //stored-- each time
-                if (stored == 0){
-                    taskComplete =true;
-                    nextTask = 1;
+                PIDDrive.setTargetState(odometry.getXCoordinate(), 132, 0);
+                if (PIDDrive.distanceToTarget() < 0.1){
+                    PIDDrive.setTargetState(130, 132, 0);
 
                 }
-            }
-             //help how do i
-        }
-
-
-        if (taskComplete == true) {
-            odometry.updatePosition();
-            PIDDrive.updatePID();
-            if (PIDDrive.distanceToTarget < 0.1) { //when we reach the desired point, then our next point is queued
-                numberOfPointsReached++;
-            }
-            if (numberOfPointsReached % 2 != 0 /*on the pixel stacks, change this later */ && distSensor.getDistance(DistanceUnit.INCH) + 0.1 > 12 || distSensor.getDistance(DistanceUnit.INCH) - 0.1 < 12) { //change this later
-                PIDDrive.setTargetState(12 - distSensor.getDistance(DistanceUnit.INCH), odometry.getYCoordinate(), 0);
-                overriding = true;
-            }
-            if (numberOfPointsReached % 2 != 0 /*on the backstage stacks, change this later */ && distSensor.getDistance(DistanceUnit.INCH) + 0.1 > 12 || distSensor.getDistance(DistanceUnit.INCH) - 0.1 < 12) { //change this later
-                PIDDrive.setTargetState(12 - distSensor.getDistance(DistanceUnit.INCH), odometry.getYCoordinate(), 0);
-                overriding = true;
-            }
-            //above is the exact same thing but when on backstage
-            if (numberOfPointsReached < targetStates.length) { //movement
-                if (overriding == false) {
-                    PIDDrive.setTargetState(targetStates[numberOfPointsReached][0], targetStates[numberOfPointsReached][1], targetStates[numberOfPointsReached][2]);
-                }
-
 
             }
 
         }
-        }
+    }
     }
