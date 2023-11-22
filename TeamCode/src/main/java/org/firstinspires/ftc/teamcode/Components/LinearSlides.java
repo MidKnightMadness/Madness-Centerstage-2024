aabd7f730a85479e16115c498675236325923722
mike-wen1 <torchmaster123@gmail.com>
2023-11-21 21:47:38 -0800

    Added IMU to odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/LinearSlides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/LinearSlides.java
@@ -15,6 +15,9 @@ public class LinearSlides {
     public double inPerTickRight = 0.0;
     public double extensionDifferenceTolerance = 0.0;
     public double [] extendedLengths = {0.0, 0.0}; // Left, right
+    public double [] powerConstants = {0.0, 0.0};
+    double correctionConstant = 0.0;
+    public final double LENGTH_TOLERANCE = 0.1; // inches
 
     public LinearSlides(HardwareMap hardwareMap) {
         motorLeft = hardwareMap.get(DcMotorEx.class, "Left outtake motor");
@@ -31,10 +34,18 @@ public class LinearSlides {
         motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    public void setHeight(double height) { // Run every tick when you want motors to go to this
-//        while (Math.abs(extendedLengths [0] - extendedLengths [1]) < extensionDifferenceTolerance && ) {
-//
-//        }
+    public void setHeight(double targetLength) { // Run every tick when you want motors to go to this
+        while (Math.abs(extendedLengths [0] - extendedLengths [1]) < extensionDifferenceTolerance && (extendedLengths [0] + extendedLengths [1]) / 2.0d < LENGTH_TOLERANCE &&
+                    leftBounds [0] <= extendedLengths [0] && extendedLengths [0] <= leftBounds [1] &&
+                    rightBounds [0] <= extendedLengths [1] && extendedLengths [1] <= rightBounds [1]) {
+            // Using left side as leader
+            motorLeft.setPower((targetLength - extendedLengths [0]) * powerConstants [0]);
+            motorRight.setPower((targetLength - extendedLengths [1]) * powerConstants [1] +
+                    (extendedLengths [0] - extendedLengths [1]) * correctionConstant);
+
+            extendedLengths [0] = inPerTickLeft * motorLeft.getCurrentPosition();
+            extendedLengths [1] = inPerTickRight * motorRight.getCurrentPosition();
+        }
     }
 
 }
